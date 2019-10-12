38a6454f0984fd9c72677a943145a0ccfd10dded
Alexander Lavrukhine <zanhesl@gmail.com>
2019-10-12 21:22:42 +0300

    feat: pass all the tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this._left = min;
+      this._right = max;
+      this._guess = Math.ceil((this._right + this._left) / 2)
     }
 
     guess() {
-
+      return this._guess;
     }
 
     lower() {
-
+      this._right = this._guess;
+      this._guess = Math.ceil((this._right + this._left) / 2)
     }
 
     greater() {
-
+      this._left = this._guess;
+      this._guess = Math.ceil((this._right + this._left) / 2);
     }
 }
 
